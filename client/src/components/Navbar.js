1bfe808769475dab94e47920ec918bb8233432ce
Sri <srilalitha.ganti@gmail.com>
2022-02-05 14:55:17 +1100

    made navbar responsive

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,32 +1,57 @@
-import React from "react";
-import "../App.css";
+import React, { useState } from "react";
+
 import { Link } from "react-router-dom";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes, faBars } from "@fortawesome/free-solid-svg-icons";
+
+
 function Navbar() {
+  const iconTimes = <FontAwesomeIcon icon={faTimes} />;
+  const iconBars = <FontAwesomeIcon icon={faBars} />;
+
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
-    <div>
-      <nav className="topnav" id="myTopnav">
-        <h4 className=" title">Srinivasa Skin and General Hospital</h4>
-        <ul>
-          <li>
-            <Link to={"/"} className="link">
+    <div className="in-line">
+      <nav className="navbar1">
+        <Link to="/" className="navbar1" onClick={closeMobileMenu}>
+          <h2>Srinivasa Skin and General Hospital</h2>
+        </Link>
+      </nav>
+      <nav className="navbar">
+        <div className="menu-icon" onClick={handleClick}>
+          <i>{click ? iconTimes : iconBars} </i>
+          <i className={click ? "fas fa-time" : "fas fa-bars"} />
+        </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
               Home
             </Link>
           </li>
-          <li>
-            <Link to={"/bookings/"} className="link">
+          <li className="nav-item">
+            <Link
+              to="/bookings"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
               Bookings
             </Link>
           </li>
-          <li>
-            <Link to={"/contact"} className="link">
+          <li className="nav-item">
+            <Link to="/contact" className="nav-links" onClick={closeMobileMenu}>
               Contact
             </Link>
           </li>
-       
+
         </ul>
       </nav>
     </div>
   );
 }
+
 export default Navbar;
