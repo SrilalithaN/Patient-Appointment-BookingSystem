b90d70a495278c76235e9370e2a7f22f14bffbc5
Sri <srilalitha.ganti@gmail.com>
2022-02-07 18:00:21 +1100

    error deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,9 +3,6 @@ mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/patient-booking-system",
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
   }
 );
 module.exports = mongoose.connection;
