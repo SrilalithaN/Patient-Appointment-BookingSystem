4a2784e6d19b9bc9cda33a25d41519206d5e5e86
Sri <srilalitha.ganti@gmail.com>
2022-02-07 17:46:48 +1100

    heroku deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/patient-booking-system",
   {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
   }
 );
 module.exports = mongoose.connection;
