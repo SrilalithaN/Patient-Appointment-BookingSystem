0b26652b20c82842e37eb924216c24087429d66a
Sri <srilalitha.ganti@gmail.com>
2022-02-12 08:45:08 +1100

    need to refresh page to view appointments

--- client/src/components/Patient.js
+++ client/src/components/Patient.js
@@ -1,25 +1,22 @@
 import React from "react";
-import { useQuery ,useMutation } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
 import { GET_PATIENTINFO } from "../utils/queries";
 import { FormatDate } from "../utils/helpers";
 import { Card, Button } from "semantic-ui-react";
 import Navbar from "./Navbar";
 import { CANCEL_APPOINTMENT } from "../utils/mutations";
 
-
 const PatientData = () => {
   const { loading, data } = useQuery(GET_PATIENTINFO);
 
   const patient = data?.patient || [];
 
-
   const [cancelAppointment] = useMutation(CANCEL_APPOINTMENT, {
     update(cache, { data: { cancelAppointment } }) {
       try {
-    
         cache.writeQuery({
           query: GET_PATIENTINFO,
-          data: { patient:cancelAppointment },
+          data: { patient: cancelAppointment },
         });
       } catch (e) {
         console.error(e);
@@ -29,10 +26,9 @@ const PatientData = () => {
 
   const handleDeleteAppointment = async (bookingId) => {
     try {
-    await cancelAppointment({
+      await cancelAppointment({
         variables: { bookingId },
       });
-    
     } catch (error) {}
   };
   if (loading) {
@@ -42,10 +38,11 @@ const PatientData = () => {
     <div>
       <Navbar />
       <div className="container">
-        <h2 className="patientCard" >
+        <h2 className="patientCard">
           Hello <span className="user">{patient.fullName}!</span>
         </h2>
-        <h2 className="patientCard" >
+        <p>Please refresh the page to view new appointments.</p>
+        <h2 className="patientCard">
           {patient.appointments.length
             ? "Thank you for booking appointment with us"
             : "You have no appointments to view"}
@@ -53,34 +50,34 @@ const PatientData = () => {
 
         {patient.appointments.map((appointment) => {
           return (
-            <div className="patientCard" >
-            <Card.Group itemsPerRow={2} key = {appointment._id}>
-              <Card >
-                <Card.Description>
-                  <div className="patientInfo">
-                    <p className="patientInfo">
-                      Patient Name: {appointment.patientName}
-                    </p>
-                    <p className="patientInfo">
-                      Doctor Name: {appointment.doctorName}
-                    </p>
-                    <p className="patientInfo">
-                      Date and Time of Appointment:
-                      {FormatDate(appointment.dateTime)}
-                    </p>
-                    <Button
-                      color="blue"
-                      type="submit"
-                      size="large"
-                      value={appointment._id}
-                      onClick={() => handleDeleteAppointment(appointment._id)}
-                    >
-                      Cancel Appointment
-                    </Button>
-                  </div>
-                </Card.Description>
-              </Card>
-            </Card.Group>
+            <div className="patientCard">
+              <Card.Group itemsPerRow={2} key={appointment._id}>
+                <Card>
+                  <Card.Description>
+                    <div className="patientInfo">
+                      <p className="patientInfo">
+                        Patient Name: {appointment.patientName}
+                      </p>
+                      <p className="patientInfo">
+                        Doctor Name: {appointment.doctorName}
+                      </p>
+                      <p className="patientInfo">
+                        Date and Time of Appointment:
+                        {FormatDate(appointment.dateTime)}
+                      </p>
+                      <Button
+                        color="blue"
+                        type="submit"
+                        size="large"
+                        value={appointment._id}
+                        onClick={() => handleDeleteAppointment(appointment._id)}
+                      >
+                        Cancel Appointment
+                      </Button>
+                    </div>
+                  </Card.Description>
+                </Card>
+              </Card.Group>
             </div>
           );
         })}
